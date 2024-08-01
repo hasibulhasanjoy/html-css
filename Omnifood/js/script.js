8932d8584d0c812a9caedf94337ae9354d607910
Hasibul <hasibulhasanjoy.007@gmail.com>
2024-08-01 08:36:21 +0600

    implement smooth scrool using html

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+const sectionHero = document.querySelector(".section-hero");
+const sectionHow = document.querySelector(".how-it-works");
+const sectionMeals = document.querySelector(".section-meals");
+const sectionTestimonial = document.querySelector(".section-testimonial");
+const sectionPricing = document.querySelector(".section-price");
+const sectionCTA = document.querySelector(".section-cta");
+const btnHero = document.querySelector(".btn--full");
+
 const makeMobileNavWork = function () {
   const header = document.querySelector(".header");
 
