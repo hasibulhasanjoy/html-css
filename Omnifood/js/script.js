66f8938c1439440c43a09c765dbe51ddf4fff650
Hasibul <hasibulhasanjoy.007@gmail.com>
2024-08-01 09:21:12 +0600

    impelement smooth scrooll using js

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -1,12 +1,39 @@
 "use strict";
 
+const sectionHeader = document.querySelector("header");
 const sectionHero = document.querySelector(".section-hero");
-const sectionHow = document.querySelector(".how-it-works");
-const sectionMeals = document.querySelector(".section-meals");
-const sectionTestimonial = document.querySelector(".section-testimonial");
-const sectionPricing = document.querySelector(".section-price");
-const sectionCTA = document.querySelector(".section-cta");
-const btnHero = document.querySelector(".btn--full");
+const sectionHow = document.querySelector("#how");
+const sectionMeals = document.querySelector("#meals");
+const sectionTestimonial = document.querySelector("#testimonial");
+const sectionPricing = document.querySelector("#price");
+const sectionCTA = document.querySelector("#cta");
+const btnCTA = document.querySelector(".btn--full");
+const btnLearnMore = document.querySelector(".btn--outline");
+const links = document.querySelectorAll("a:link");
+
+// smooth scrolling //
+links.forEach((link) => {
+  if (
+    link.classList.contains("main-nav-link") ||
+    link.classList.contains("btn--full") ||
+    link.classList.contains("btn--outline") ||
+    link.classList.contains("footer-logo")
+  ) {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log(link);
+      const targetSection = link.getAttribute("href");
+      let targetSectionElement = "";
+      if (targetSection != "#") {
+        targetSectionElement = document.querySelector(targetSection);
+      } else {
+        targetSectionElement = sectionHeader;
+      }
+      targetSectionElement.scrollIntoView({ behavior: "smooth" });
+      sectionHeader.classList.remove("nav-open");
+    });
+  }
+});
 
 const makeMobileNavWork = function () {
   const header = document.querySelector(".header");
